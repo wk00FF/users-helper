14ee7724a7f23a8f24fe15cc934e474f20e6b469
william smith <wk00FF@gmail.com>
2024-09-16 05:57:11 +0500

    + composer + описания

--- src/UsersHelper.php
+++ src/UsersHelper.php
@@ -3,6 +3,13 @@ namespace wk00FF;
 class UsersHelper{
 	protected $ID;
 	protected $fields=[];
+
+	/**
+	 * @param int $userID		если его нет, то будет взят текущий юзер
+	 * @param array $fields		$fields и $uf_fields: если false, то скачает все поля, если [] - то ниодного
+	 * @param array $uf_fields
+	 * @throws \Bitrix\Main\ArgumentException
+	 */
 	public function __construct(int $userID=0, array $fields=['*'], array $uf_fields=['UF*']){
 		global $USER;
 		if($userID){
@@ -12,43 +19,48 @@ class UsersHelper{
 			$this->ID=$USER->GetID();
 		}
 		$this->fields=self::getByID($this->ID, $fields, $uf_fields);
+		return $this;
 	}
+	/**
+	 * получить поля из объекта
+	 * @param array $fields
+	 * @return array|false|mixed|string[]
+	 */
 	public function getFields(array $fields=[]){
-		if($fields){
-			if(!is_array($fields)){
-				if($fields!='') $fields=array($fields);
+		if(count($fields)){
+			$ret=[];
+			foreach($fields as $field){
+				$ret[$field]=$this->getField($field);
 			}
-			if(count($fields)>0){
-				$ret=[];
-				foreach($fields as $field){
-					$ret[$field]=$this->getField($field);
-				}
-				return $ret;
-			}
+			return $ret;
 		}
 		return $this->fields;
 	}
+	/**
+	 * получить поле из объекта
+	 * @param $field
+	 * @return mixed|string
+	 */
 	public function getField($field){
 		return $this->fields[$field];
 	}
-
-	public static function getByID(int $userID=0, $fields=['*'], $uf_fields=['UF*']){
+	/**
+	 * получает значение полей юзера
+	 * @param int $userID					если его нет, то будет взят текущий юзер
+	 * @param array $fields					$fields и $uf_fields: если false, то скачает все поля, если [] - то ниодного
+	 * @param array $uf_fields
+	 * @return array|false|mixed|string[]	вернет массив с полями. Если в запрашиваемых полях есть GROUPS, там будут группы, в которых состоит юзер
+	 * @throws \Bitrix\Main\ArgumentException
+	 */
+	public static function getByID(int $userID=0, array $fields=['*'], array $uf_fields=['UF*']){
 		global $USER;
 		$ID=$userID?:$USER->GetID();
 		$select=[];
 		if(is_array($fields)){
 			$select=array_merge($fields);
 		}
-		else{
-			if($fields===false) $fields='*';
-			if($fields!='') $select[]=$fields;
-		}
 		if(is_array($uf_fields)){
 			$select=array_merge($uf_fields);
-		}
-		else{
-			if($uf_fields===false) $uf_fields='UF*';
-			if($uf_fields!='') $select[]=$uf_fields;
 		}
 		if(count($select)){
 			$fields=\Bitrix\Main\UserTable::getList(['select'=>$select, 'filter'=>['ID'=>$ID]])->fetch();
@@ -58,6 +70,12 @@ class UsersHelper{
 		}
 		return $fields?:false;
 	}
+	/**
+	 * запись данных
+	 * @param int $userID
+	 * @param array $fields		массив с полями. Если там есть GROUPS, то юзер будет назначен ан эти группы
+	 * @return void
+	 */
 	public static function setFieldsByID(int $userID=0, array $fields){
 		global $USER;
 		$ID=$userID?:$USER->GetID();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wk00ff/users-helper',
+        'pretty_version' => '1.000',
+        'version' => '1.000.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wk00ff/users-helper' => array(
+            'pretty_version' => '1.000',
+            'version' => '1.000.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
